41a82029c1062e5995559d30859115b98a8925eb
zaan <jaroslaw.miazga@gmail.com>
2012-05-16 10:39:38 +0200

    Polish locale

---
+++ js/locale/ui-multiselect-pl.js
@@ -0,0 +1,11 @@
+/**
+ * Localization strings for the UI Multiselect widget
+ *
+ * @locale pl
+ */
+
+$.extend($.ui.multiselect.locale, {
+	addAll:'Dodaj wszystkie',
+	removeAll:'Usuń wszystkie',
+	itemsCount:'wybrane elementy'
+});
